61032c370a8430f5a18d68cdebb723ef7aca3c31
Peter Fessel <peter.fessel@viasto.com>
2014-06-18 16:05:47 +0200

    'translate' ember app to ember app kit; works

--- ember-app-kit/app/router.js
+++ ember-app-kit/app/router.js
@@ -1,11 +1,23 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
+
   this.route('component-test');
   this.route('helper-test');
-  // this.resource('posts', function() {
-  //   this.route('new');
-  // });
+  
+  this.resource('first-section', function () {
+  	this.route('part-1');
+  	this.route('part-2');
+  	this.route('part-3');
+  });
+
+  this.resource('another-section', function () {
+  	this.route('part-1');
+  	this.route('part-2');
+  	this.route('part-3');
+  });
+
+  this.resource('about');
 });
 
 export default Router;
