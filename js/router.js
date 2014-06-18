73a2c524702c78a10402877d951fe1fd3fa5bab9
Peter Fessel <peter.fessel@viasto.com>
2014-06-18 11:04:41 +0200

    add routers for first navigation layer pages; not working now due to 'connectOutlet' error

--- js/router.js
+++ js/router.js
@@ -16,3 +16,33 @@ App.Router.map(function() {
 
   this.resource('about');
 });
+
+App.FirstSectionRoute = Ember.Route.extend({
+  model: function () {},
+  renderTemplate: function() {
+    this.render('first-section', {
+      into: 'main-app-layout',
+      outlet: 'main-content'
+    });
+  }
+});
+
+App.AnotherSectionRoute = Ember.Route.extend({
+  model: function () {},
+  renderTemplate: function() {
+    this.render('another-section', {
+      into: 'main-app-layout',
+      outlet: 'main-content'
+    });
+  }
+});
+
+App.AboutRoute = Ember.Route.extend({
+  model: function () {},
+  renderTemplate: function() {
+    this.render('about', {
+      into: 'main-app-layout',
+      outlet: 'main-content'
+    });
+  }
+});
